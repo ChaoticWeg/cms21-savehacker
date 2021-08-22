7e42722ca067a7d2b18d4f329ef689ac875f5ba4
ChaoticWeg <shawn@chaoticweg.cc>
2021-08-22 17:42:53 -0500

    Add electron

---
+++ public/electron.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+
+const path = require("path");
+
+const { app, BrowserWindow } = require("electron");
+const isDev = require("electron-is-dev");
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    const url = isDev ? `http://localhost:3000/${process.env.PUBLIC_URL}` : `file://${path.join(__dirname, "../build/index.html")}`;
+    win.loadURL(url);
+
+    if (isDev) {
+        win.webContents.openDevTools({ mode: "detach" });
+    }
+}
+
+function onActivate() {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+}
+
+function onAllWindowsClosed() {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+}
+
+app.whenReady().then(createWindow);
+app.on("activate", onActivate);
+app.on("window-all-closed", onAllWindowsClosed);
